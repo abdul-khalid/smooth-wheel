b62b908913af47d1c40437f0c61433a009506f41
qixiang.chen <qixiang.chen@gmail.com>
2011-04-01 17:39:19 +0000

    Initial commit.

---
+++ smoothWheel/src/com/sinkpoint/smoothwheel/wsMainActivity.java
@@ -0,0 +1,20 @@
+package com.sinkpoint.smoothwheel;
+
+import com.sinkpoint.wheelspinner.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class wsMainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        IntWheelAdapter wadp = new IntWheelAdapter(0, 9, "00");
+        setContentView(R.layout.main);
+        
+        WheelView wv = (WheelView) this.findViewById(R.id.wheel);
+        wv.setAdapter(wadp);        
+        
+    }
+}
